9057da47286af0265589ac8b7ff073b3996fbbf1
Olya <belyaevaolga881@gmail.com>
2021-03-21 03:53:32 +0300

    max min

--- Lab2_V2_1/V2DataOnGrid.cs
+++ Lab2_V2_1/V2DataOnGrid.cs
@@ -13,6 +13,48 @@ namespace ClassLibrary
         public Grid1D[] Grids { get; set; }
         public Complex[,] Node { get; set; }
 
+        public double Max
+        {
+            get
+            {
+                double ret = 0;
+                if (Grids[0].Num > 0 && Grids[1].Num > 0)
+                {
+                    ret = Node[0, 0].Magnitude;
+                    for (int i = 0; i < Grids[0].Num; i++)
+                    {
+                        for (int j = 0; j < Grids[1].Num; j++)
+                        {
+                            if (ret < Node[i, j].Magnitude)
+                                ret = Node[i, j].Magnitude;
+                        }
+                    }
+                }
+                return ret;
+            }
+        }
+
+        public double Min
+        {
+            get
+            {
+                double ret = 0;
+                if (Grids[0].Num > 0 && Grids[1].Num > 0)
+                {
+                    ret = Node[0, 0].Magnitude;
+                    for (int i = 0; i < Grids[0].Num; i++)
+                    {
+                        for (int j = 0; j < Grids[1].Num; j++)
+                        {
+                            if (ret > Node[i, j].Magnitude)
+                                ret = Node[i, j].Magnitude;
+                        }
+                    }
+                }
+                return ret;
+            }
+        }
+
         public V2DataOnGrid(string info, double freq, Grid1D ox, Grid1D oy) : base(info, freq)
         {
             Info = info;
